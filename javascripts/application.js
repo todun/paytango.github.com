fc66fb9c54f336d43b21852093b839205dc9eb2e
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-09-17 15:47:11 -0400

    email hover

---
+++ javascripts/application.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('button').hover(function(){
+    $(this).html("<a style=\"font-size: 18px;\"href=\"mailto:tangopayments@gmail.com\">tangopayments@gmail.com</a>")
+  }, function(){
+    $(this).html("<a  href=\"mailto:tangopayments@gmail.com\">CONTACT US</a>")
+  })
+});
